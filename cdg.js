84b9e2c5fce354ce83c13460489f09ad28e8e093
AnnefranceFBC <71523877+AnnefranceFBC@users.noreply.github.com>
2021-09-23 12:07:22 -0300

    comebak to reasoning

--- cdg.js
+++ cdg.js
@@ -1,5 +1,12 @@
 var anne = {nome:"Anne", vitorias:4, empates:3, derrotas:1, pontos:0};
 var heitor = {nome:"Heitor", vitorias:5, empates:2, derrotas:3, pontos:0};
 
-console.log(heitor);
-console.log(anne);
+calculaPontos(anne);
+
+
+function calculaPontos(jogador) {
+    var pontos = jogador.vitorias * 3 + jogador.empates;
+    console.log(pontos);
+}
+
+
