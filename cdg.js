20033ba0cdafa2e8a53361f87dd2ab4ed15c71e7
AnnefranceFBC <71523877+AnnefranceFBC@users.noreply.github.com>
2021-09-23 10:48:40 -0300

    half project

--- cdg.js
+++ cdg.js
@@ -1,1 +1,5 @@
-alert('Hello World');
+var anne = {nome:"Anne", vitorias:4, empates:3, derrotas:1, pontos:0};
+var heitor = {nome:"Heitor", vitorias:5, empates:2, derrotas:3, pontos:0};
+
+console.log(heitor);
+console.log(anne);
